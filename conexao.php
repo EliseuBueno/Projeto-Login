f27c3c056e64a9a2312df213bb5d26f3c20d59d2
EliseuBueno <59027014+EliseuBueno@users.noreply.github.com>
2022-01-12 00:21:21 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -6,9 +6,3 @@ $dbname = "loja";
 
 //Criar a conexao
 $conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
-
-
-//$servidor = "sql304.epizy.com";
-//$usuario = "epiz_30215393";
-//$senha = "DNfHiJmD2Oe";
-//$dbname = "epiz_30215393_loja";
